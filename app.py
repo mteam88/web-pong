bc02e3c77c6dc03d90dddd2f9457c4c3f2fb3772
mteam88 <matthew8edelen@gmail.com>
2022-06-22 12:00:30 -0400

    done

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import json
+import random
 app = Flask(__name__)
 
 playerskin = "static/player/whiteplayer.png"
+ballskins = ["static/ball/ball1.png", "static/ball/blueball.png", "static/ball/redball.png"]
 
 @app.route("/")
 def startscreen():
@@ -11,7 +13,7 @@ def startscreen():
 @app.route("/pong")
 def pong():
     print(playerskin)
-    return render_template("index.html", data={"playerskin": playerskin})
+    return render_template("index.html", data={"playerskin": playerskin, 'ballskin': random.choice(ballskins)})
 
 @app.route("/gameover/<winner>")
 def gameover(winner):
