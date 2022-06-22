71c28c988e95fc70e394708ca65efd59e60843e5
mteam88 <matthew8edelen@gmail.com>
2022-06-21 21:59:02 -0400

    .

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def startscreen():
+    return render_template("start.html")
+
+@app.route("/pong")
+def pong():
     return render_template("index.html")
+
+@app.route("/gameover/<winner>")
+def gameover():
+    return render_template("gameover.html", data={"winner": winner})
 
 app.run(debug=True)
